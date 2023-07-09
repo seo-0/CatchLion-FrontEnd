04c97b50aadf4e140407a6bbe1666478dd4c45ac
castlehuni <osh8137@naver.com>
2023-07-10 00:00:51 +0900

    detail서버연결준비&register디자인일부변경

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,8 @@
 // import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import styled from "styled-components";
 import "./Detail.css";
 
@@ -11,8 +15,36 @@ const Btndiv = styled.div`
     margin: 0 auto;
     text-align: center;
 `
+const Deadlinediv = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+    font-weight: bold;
+`
 
 const Detail=()=>{
+    const { jobId } = useParams();
+    const [recruInfo, setRecruInfo] = useState([]);
+
+    useEffect(() => {
+        const fetchRecruData = async () => {
+            try {
+              const response = await axios.get(`https://jsonplaceholder.typicode.com/users/`);
+              setRecruInfo(response.data);
+            } catch (error) {
+              console.error("회사 상세 정보를 가져오는 도중 에러 발생",error);
+              setRecruInfo(null);
+            }
+        };
+        
+        fetchRecruData();
+
+    }, [jobId]);
+
+    if (!recruInfo) {
+        return <div>회사 상세 정보를 불러오는중</div>;
+      }
+
     return(
         <div>
             <Title>
@@ -31,11 +63,20 @@ const Detail=()=>{
                 </div>
                 <div className="Coinfo">
                     <p className="info">기업정보</p>
+                    <p>회사 이름: {recruInfo.name}</p>
+                    <p>회사 위치: {recruInfo.username}</p>
                 </div>
             </div><br/>
             <Btndiv>
-                <button>즉시 지원</button>
+                <Link to="/register">
+                    <button id="detailbtn">즉시 지원</button>
+                </Link>
             </Btndiv>
+            <Deadlinediv>
+                <p>해당 회사의 서류 마감일은 
+                    <span style={{color: "rgb(249, 165, 7)", fontWeight: "bold"}}>{recruInfo.email}</span>입니다.
+                </p>
+            </Deadlinediv>
         </div>
     );
 }
