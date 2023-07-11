927481561a38f361486784c802cd4d971ab681fc
castlehuni <osh8137@naver.com>
2023-07-11 23:30:14 +0900

    일부 오류 및 css 수정

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route path="/Detail/:jobId" element ={<Detail />} />
           <Route path="/Register" element ={<Register />} />
           <Route path="/Login" element ={<Login />} />
-          <Route path="/Signin" element={Signin } />
+          <Route path="/Signin" element={<Signin />} />
         </Routes>
     </div>
   );
